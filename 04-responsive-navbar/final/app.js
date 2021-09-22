26345481b49f880fdf13c3bfb11877e26a978325
Srikanth <sri1@publicisgroupe.net>
2021-09-22 10:57:25 +0530

    navbar old implementation added in js

--- 04-responsive-navbar/final/app.js
+++ 04-responsive-navbar/final/app.js
@@ -5,5 +5,11 @@ const links = document.querySelector(".links");
 
 // event on navToggle button
 navToggle.addEventListener("click", function () {
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+
   links.classList.toggle("show-links");
 });
